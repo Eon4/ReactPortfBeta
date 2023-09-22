ccf9dbfaabf85b46d7096fe2aa9b4a70946049fe
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-22 11:02:14 +0200

    building

--- portfolie/src/App.jsx
+++ portfolie/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainLayout } from "./layout/MainLayout";
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
-
   return (
-    <>
-      <h1 >
-Lets build something cool
-      </h1>
-    </>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout/>}>
+            <Route index element={<HomePage/>}/>
+            {/* <Route path="/about" element={<AboutPage/>}/> */}
+            {/* <Route path="*" element={<NotFoundPage/>}/> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ portfolie/src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import style from './Nav.module.scss'
+import { Link } from 'react-router-dom';
+
+export const Navigation = () => {
+ 
+    useEffect(() => {
+        console.log("Navigation Component has Mounted")
+        return (
+            () => {
+                console.log("Navigation Component has Un-Mounted")
+            }
+        )
+    }, [])
+
+    return (
+        <nav className={style.navStyle}>
+            <div>
+                <Link to="/">Home</Link>
+                {/* <Link to="/about">About</Link> */}
+            </div>
+        </nav>
+    )
+}
