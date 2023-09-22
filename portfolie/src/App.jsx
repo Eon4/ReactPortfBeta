fbdb9734581a1c2c3e14666ad42d3103f554f17d
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-22 11:23:10 +0200

    building and connection routing

--- portfolie/src/App.jsx
+++ portfolie/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layout/MainLayout";
-import { HomePage } from "./pages/HomePage";
-import { AboutPage } from "./pages/AboutPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
+import { HomePage } from "./pages/HomePage/HomePage";
+// import { AboutPage } from "./pages/AboutPage";
+// import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
